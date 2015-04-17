4ca06c67cb9446c2af8a21707dac733bc67efc47
chencheng <sorrycc@gmail.com>
2015-04-17 11:09:40 +0800

    let es6 support ie8

--- es6/index.js
+++ es6/index.js
@@ -1,3 +1,20 @@
 
+class Model {
+  constructor() {}
+}
+
+class User extends Model {
+  constructor() {
+    super();
+    this.sayHello();
+  }
+
+  sayHello() {
+    alert("Hello World!");
+  }
+}
+
 import B from './b';
-alert(B);
+// alert(B);
+
+new User();
